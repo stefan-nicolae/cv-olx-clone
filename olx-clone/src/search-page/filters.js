9acc9a381f471ca514aaa8a9e7e1598b6fef640e
stef11111 <84786481+stefan-nicolae@users.noreply.github.com>
2022-10-25 20:02:29 +0300

    update

--- olx-clone/src/search-page/filters.js
+++ olx-clone/src/search-page/filters.js
@@ -7,6 +7,31 @@ export default function Filters (props) {
     selected["categorie"] = props.searchParams.categorie ? decodeURIComponent(props.searchParams.categorie) : undefined
     selected["firma"] = props.searchParams.firma ? decodeURIComponent(props.searchParams.firma).replaceAll("+", " ").replaceAll("_", " ").toLowerCase() : undefined
     selected["sorteaza"] = props.searchParams.sorteaza 
+    selected["minim"] = props.searchParams.minim
+    selected["maxim"] = props.searchParams.maxim
+
+    const submitForm = (e) => {
+        console.log(e[0].value)
+        const formData = {}
+        for(let i = 0; i <= 2; i++) {
+            formData[e[i].name] = e[i].value !== "placeholder" ? e[i].value : undefined
+        }
+
+        for(let i = 3; i <= 4; i++) {
+            formData[e[i].name] = e[i].value ? e[i].value : undefined
+        }       
+
+        if(e[0].value && e[1].value && e[0].value !== "placeholder" && e[1].value !== "placeholder") {
+            const brands = []
+            
+            props.data.categories[e[0].value].forEach(product => {
+                brands.push(product.brand.toLowerCase().replaceAll("_", " ").toLowerCase())
+            })  
+            if(!brands.includes(e[1].value)) formData["firma"] = undefined
+        }
+
+         props.filteredSearch(formData)
+    }
 
     const dropdowns = []
     const brands = !selected["categorie"] ? JSON.parse(window.localStorage.getItem("allBrands")).sort() : (() => {
@@ -14,23 +39,45 @@ export default function Filters (props) {
         props.data.categories[selected["categorie"]].forEach(product => {
             brands.push(product.brand.toLowerCase().replaceAll("_", " ").toLowerCase())
         })  
-        const uniqueBrands = [...new Set(brands.sort())]
-        if(selected["firma"] && !uniqueBrands.includes(selected["firma"])) props.filteredSearch({"firma": undefined})
-        return uniqueBrands
+        return [...new Set(brands.sort())]
     })()
 
     const categories = Object.keys(props.data.categories).sort()
-    dropdowns.push(["categorie", categories], ["firma", brands], ["sorteaza", ["Scump", "Ieftin", "Noi"]])
+    dropdowns.push(["categorie", categories], ["firma", brands], ["sorteaza", ["Scumpe", "Ieftine", "Noi"]])
     
 
     console.log(selected)
+    
+    const handlePriceInputKeys = (e) => {
+        const regex=/^[0-9]+$/
+        if(!e.key.match(regex) && e.key !== "Backspace" && e.key !== "Enter") e.preventDefault()
+        if(e.key === "Enter") {
+            submitForm(e.target.parentElement.parentElement.parentElement)
+        }
+    }
+
+    const handlePriceInputChange = (e) => {
+        const current = e.target
+        const other = document.querySelector(`input[name="${current.name !== "maxim" ? "maxim" : "minim"}"]`)
+        if(current.value !== "" && isNaN(parseFloat(current.value))) current.value = "" 
+
+        if(current.value !== "" && other.value !== "") {
+            if(current.name === "minim" && current.value > other.value) other.value = current.value
+            if(current.name === "maxim" && current.value < other.value) other.value = current.value
+        }
+        setTimeout(() => {
+            submitForm(e.target.parentElement.parentElement.parentElement)
+        }, 2000)
+    }
 
     let key1=0, key2=0
+
+
     return (
         <form className="filters">
             {
                 dropdowns.map(dropdown => {
-                    return(<div key={key1++}className="dropdown">
+                    return(<div key={key1++} className="dropdown">
                         <label htmlFor={dropdown[0]}>{capitalize(dropdown[0])}</label>
 
                         <select style={
@@ -51,7 +98,7 @@ export default function Filters (props) {
                                 // dropdown[0] === "sorteaza" ? {display: "none"} : {}
                                 {}
                             } onClick={(e) => [
-                                e.target.parentElement.parentElement.parentElement.submit()
+                                submitForm(e.target.parentElement.parentElement.parentElement)
                             ]} value={"placeholder"}>
                                 {/* Toate */}
                                 {dropdown[0] === "sorteaza" ? "Fara sortare" : "Toate"}
@@ -61,7 +108,7 @@ export default function Filters (props) {
                                 dropdown[1].map(option => {
                                     const optionShown = capitalize(option.replaceAll(dropdown[0] === "Firma" ? " " : "-", " ").replaceAll("_", " "))
                                     return(<option onClick={(e) => {
-                                        e.target.parentElement.parentElement.parentElement.submit()
+                                        submitForm(e.target.parentElement.parentElement.parentElement)
                                     }} key={key2++} value={
                                         option
                                     }>{optionShown}</option>)
@@ -74,7 +121,8 @@ export default function Filters (props) {
             }
             <div className="dropdown"><span>Limita pret</span>
                 <div className="inputs">
-                    <input type="number"></input><input type="number"></input>
+                    <input defaultValue={selected["minim"]} onChange={(e) => {handlePriceInputChange(e) }} onKeyDown={(e) => {handlePriceInputKeys(e)}} name="minim" placeholder="Minim"></input>
+                    <input defaultValue={selected["maxim"]} onChange={(e) => {handlePriceInputChange(e) }} onKeyDown={(e) => {handlePriceInputKeys(e)}} name="maxim" placeholder="Maxim"></input>
                 </div>
             </div>
         </form>
